c0e90c560ece5e9b1a4435d1e5837b4c7064f819
alexMolko <carlosmayarodriguez@gmail.com>
2021-04-19 08:53:19 -0500

    creating components to be used

---
+++ src/app/contact-book/contact-add/contact-add.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ContactAddComponent } from './contact-add.component';
+
+describe('ContactAddComponent', () => {
+  let component: ContactAddComponent;
+  let fixture: ComponentFixture<ContactAddComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ContactAddComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ContactAddComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
